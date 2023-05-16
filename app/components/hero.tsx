5d003a74a5f8405200a240ba6f5284291cdcc44f
Rick <35900587+Rmole57@users.noreply.github.com>
2023-05-16 15:28:52 -0400

    hero skeleton

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import { Box, Typography } from '@mui/material';
+
 export default function Hero() {
   return (
-    <h1>Hero</h1>
-  )
+    <Box margin="56px 100px">
+      <Typography variant="h1">Hi, I&apos;m Rick.</Typography>
+      <Typography variant="h2">
+        I used to tell stories through film, now I tell them through code. And
+        I&apos;d love to help you tell yours.
+      </Typography>
+    </Box>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+'use client';
+
 import { HeaderNav, Hero, About, Projects, Contact } from './components';
+import { Box } from '@mui/material';
 
 export default function Home() {
   return (
-    <main>
+    <Box>
       <HeaderNav />
       <Hero />
       <About />
       <Projects />
       <Contact />
-    </main>
-  )
+    </Box>
+  );
 }
