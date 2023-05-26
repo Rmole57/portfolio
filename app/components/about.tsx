0c848e38afe5248c0b4add93cc862054ca4b9f61
Rick <35900587+Rmole57@users.noreply.github.com>
2023-05-25 22:53:49 -0400

    update about description to include 'full stack'

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -26,11 +26,12 @@ export default function About() {
       <Box display="flex" flexDirection="column" gap={2}>
         <Typography variant="h2">About me</Typography>
         <Typography variant="body2">
-          I&apos;m a software engineer based in New York, NY. I have multiple
-          years of experience using a variety of technologies across the
-          development stack. As a firm believer in the notion that there is no
-          &quot;one size fits all&quot; solution, I enjoy learning and working
-          with new technologies to find the best fit for the task at hand.
+          I&apos;m a full stack software engineer based in New York, NY. I have
+          multiple years of experience using a variety of technologies across
+          the development stack. As a firm believer in the notion that there is
+          no &quot;one size fits all&quot; solution, I enjoy learning and
+          working with new technologies to find the best fit for the task at
+          hand.
         </Typography>
       </Box>
     </Box>
